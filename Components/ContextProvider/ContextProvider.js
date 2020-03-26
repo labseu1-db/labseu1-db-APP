7e0b7eb8b551ec79d6b5a385576f526fb906046c
Thorben <bender.thorben@gmail.com>
2020-03-26 16:14:45 +0100

    setError function and refactored code

--- Components/ContextProvider/ContextProvider.js
+++ Components/ContextProvider/ContextProvider.js
@@ -1,7 +1,21 @@
 import React, {Component, createContext} from 'react';
+import {AsyncStorage, Linking, StyleSheet} from 'react-native';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import firebaseConfig from '../../Firebase/firebaseConfig'
+import 'firebase/auth';
+import firebaseConfig from '../../Firebase/firebaseConfig';
+
+
+// handle atob warning in console
+import {decode, encode} from 'base-64';
+
+if (!global.btoa) {
+    global.btoa = encode;
+  }
+  
+  if (!global.atob) {
+    global.atob = decode;
+  }
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
@@ -11,15 +25,62 @@ export const Context = createContext()
 const db = firebase.firestore()
 
 class ContextProvider extends Component {
+    state = {
+        loading: false,
+        error: null,
+    }
+    saveToLocalStorage = async ({key, value}) =>  {
+      try {
+        await AsyncStorage.setItem(key, value);
+      } catch (error) {
+        this.setError(error);
+      }
+    }
+    getLocalStorage = async (key) => {
+      try {
+        let data = await AsyncStorage.getItem(key)
+        return data;
+      } catch(error) {
+        this.setError(error)
+      }
+    }
+    setError = error => {
+      this.setState({error: error});
+    }
+    /// WIP
+    // register = () => {
+    //     Linking.openURL('https://pinely.app/register')
+    // }
+    getFirestoreDataWithWhere = async request => {
+        try {
+          let ref = db
+            .collection(request.collection)
+            .where(request.SearchTerm, '==', request.data);
+          let querySnapshot = await ref.get();
+          await querySnapshot.forEach(doc => {
+            if (request.type === 'SAVE_ID') {
+              request.function({...request.items, value: doc.id})
+            } else  {
+              request.function({...request.items, value: doc.data()})
+            }
+          });
+        } catch (error) {
+          this.setError(error)
+        }
+      };
     render() {
         const {children} = this.props;
         return (
-            <Context.Provider value={{db: db}}>
+            <Context.Provider value={{firebase: firebase, getFirestoreDataWithWhere: this.getFirestoreDataWithWhere, saveToLocalStorage: this.saveToLocalStorage, setError: this.setError}}>
                 {children}
             </Context.Provider>
-        )
-    }
+        )}
 }
 
+const styles = StyleSheet.create({
+    Browser: {
+        marginTop: 20
+    }
+})
 
 export default ContextProvider;
--- Components/Login/Login.js
+++ Components/Login/Login.js
@@ -1,18 +1,47 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Button, TextInput} from 'react-native';
+import {View, StyleSheet, Button, TextInput, Text} from 'react-native';
 import {Context} from '../ContextProvider/ContextProvider'
 
 
 
 let Login = () => {
-    const value = React.useContext(Context)
+    // Context Api
+    const {firebase, getFirestoreDataWithWhere, saveToLocalStorage, setError} = React.useContext(Context)
+
+
+
+    // Hooks
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+
+
+    // function
+    const login = async (email, password) => {
+        console.log(email)
+        try {
+          let {user} = await firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password);
+          getFirestoreDataWithWhere({
+            collection: 'users',
+            SearchTerm: 'userEmail',
+            data: user.email,
+            type: 'SAVE_ID',
+            items: {key: 'uuid'},
+            function: saveToLocalStorage
+          });
+        } catch (error) {
+          setError(error)
+        }
+    };
+
+
     return (
         <View style={styles.View}>
-            <TextInput placeholder="Email" autoCompleteType="email" onChangeText={(e) => setEmail(e)} autoCapitalize='none'/>
-            <TextInput placeholder="Password" autoCompleteType="password" secureTextEntry={true} onChangeText={(e) => setPassword(e)} autoCapitalize='none'/>
-            <Button title="Login"/>
+            <TextInput value={email} placeholder="Email" autoCompleteType="email" onChangeText={(e) => setEmail(e)} autoCapitalize='none'/>
+            <TextInput value={password} placeholder="Password" autoCompleteType="password" secureTextEntry={true} onChangeText={(e) => setPassword(e)} autoCapitalize='none'/>
+            <Button title="Login" onPress={() => login(email, password)}/>
         </View>
     )
 }
